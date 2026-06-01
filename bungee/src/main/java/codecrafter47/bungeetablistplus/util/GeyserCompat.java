e23f5a9882aac7be48a1f1795ab9cf2c5e96f1b2
Finnlay.hnk <90763683+finnlayq@users.noreply.github.com>
2026-06-01 13:26:56 +0200

    Fix optional integration dependency resolution

--- bungee/src/main/java/codecrafter47/bungeetablistplus/util/GeyserCompat.java
+++ bungee/src/main/java/codecrafter47/bungeetablistplus/util/GeyserCompat.java
@@ -1,6 +1,5 @@
 package codecrafter47.bungeetablistplus.util;
 
-import org.geysermc.api.connection.Connection;
 import org.geysermc.floodgate.api.FloodgateApi;
 import org.geysermc.geyser.api.GeyserApi;
 
@@ -16,7 +15,7 @@ public class GeyserCompat {
 
         // Geyser
         try {
-            Class.forName("org.geysermc.api.connection.Connection");
+            Class.forName("org.geysermc.geyser.api.GeyserApi");
             geyserHook = new Function<UUID, Boolean>() {
                 @Override
                 public Boolean apply(UUID uuid) {
@@ -29,8 +28,7 @@ public class GeyserCompat {
                         if (instance == null) {
                             return false;
                         }
-                        Connection session = instance.connectionByUuid(uuid);
-                        return session != null;
+                        return instance.connectionByUuid(uuid) != null;
                     } catch (Throwable ignored) {
 
                     }
